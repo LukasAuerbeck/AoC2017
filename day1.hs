80e22af996466040063cadbcfc6bf66628ac46c9
Lukas Auerbeck <if15b059@technikum-wien.at>
2017-12-02 16:07:40 +0100

    day 1 both parts finished

--- day1.hs
+++ day1.hs
@@ -1,6 +1,13 @@
 compute :: String -> Int
-compute xs = compute_sub (xs ++ [head xs])  0
-    where compute_sub [x] computedSum = computedSum
-          compute_sub (x:xs) computedSum 
-            | x == (head xs) = compute_sub xs (computedSum + read [x] :: Int)
-            | otherwise = compute_sub xs computedSum
+compute xs = computeSum (xs ++ [head xs])  0
+    where computeSum [x] computedSum = computedSum
+          computeSum (x:xs) computedSum 
+            | x == (head xs) = computeSum xs (computedSum + read [x] :: Int)
+            | otherwise = computeSum xs computedSum
+
+
+compute2 xs = computeSum xs 0 (length xs `div` 2)
+    where computeSum (x:xs) computedSum compareOffset
+            | length xs < compareOffset = computedSum
+            | x == xs !! (compareOffset - 1) = computeSum xs (computedSum + (read [x] :: Int) * 2) compareOffset
+            | otherwise = computeSum xs computedSum compareOffset
